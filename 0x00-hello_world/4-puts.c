af9d83ffe88ae63c4b07315c3185b4a5e0f70c06
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-06-30 16:24:27 +0000

    betty ma pa mi na

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'C programming'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
