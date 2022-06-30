1e026a235f304edfc42f849617fbf691066d7864
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-06-30 19:53:48 +0000

    fixed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) 
+{
+	printf("\nSize of a char: %d byte(s)", sizeof(char));
+	printf("\nSize of an int: %d byte(s)", sizeof(int));
+	printf("\nSize of a long int: %d byte(s)", sizeof(long int));
+	printf("\nSize of a long long int: %d byte(s)", sizeof(long long int));
+	printf("\nSize of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
