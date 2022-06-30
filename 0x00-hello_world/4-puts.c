ef77f98752d62390459c99367e499e0d9caa5911
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-06-30 15:40:48 +0000

    Still solving

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void) {
-	char mystr[] = "Programming is like building a multilingual puzzle";
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(mystr);
 		return 0;
