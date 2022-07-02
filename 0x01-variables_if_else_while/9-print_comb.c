38b2ffdaad9fc67b1f3d09b4ac373a76f0596cf7
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-02 03:03:54 +0000

    Adding comma to iterations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -Main entry
+ *
+ * Description: 'Separating iterations by comma'
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+		putchar(',');
+		putchar(' ');
+	}
+
+	return (0);
+}
+
