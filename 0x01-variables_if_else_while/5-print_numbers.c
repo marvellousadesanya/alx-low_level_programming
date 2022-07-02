3ea6c30b9ec42c6ad8f4fdc531207be4cd1f157f
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-02 01:09:02 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing numbers
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	return (0);
+}
