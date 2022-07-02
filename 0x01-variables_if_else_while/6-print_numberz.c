55a4613c2736985794d99bc26f30ea20d84598f0
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-02 01:53:05 +0000

    Done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main -entry point
+ *
+ * Description: 'Using putchar with int data type'
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
 int main(void)
 {
 	int n;
+
 	n = 0;
 
-	while (n < 10) 
+	while (n < 10)
 	{
-		putchar(n);
+		putchar(n + '0');
 		n++;
 	}
 	putchar('\n');
