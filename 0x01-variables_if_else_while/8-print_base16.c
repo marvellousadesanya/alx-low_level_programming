f4acc7ac8e1751a03736c6bd043c3f01afb72f01
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-02 02:48:14 +0000

    Sorted

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -Entry process
+ *
+ * Description: 'Hexadecimal numbers print'
+ *
+ * Return: Always 0 (SUCCESS)
+ */
 
 int main(void)
 {
@@ -9,13 +16,13 @@ int main(void)
 	n = 0;
 	ch = 'a';
 
-	while(n < 10) 
+	while (n < 10)
 	{
 		putchar(n + '0');
 		n++;
 	}
-	
-	while(ch <= 'f') 
+
+	while (ch <= 'f')
 	{
 		putchar(ch);
 		ch++;
