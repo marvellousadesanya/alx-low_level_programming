03cfc9cf2118287d126e31eccce923fbaaf44f27
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-05 18:48:52 +0000

    hey

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void) 
+{
+	char l;
+	l = 'a';
+	while (l < 'z') 
+	{
+		_putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
