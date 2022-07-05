8dd0d82806e742b39c07c44452b1dbf6ad93c40d
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-05 18:21:22 +0000

    hey

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- *  * main - a program that prints __putchar
- *   *
- *    * Return: returns 0 always
- *     */
+ * main - a program that prints __putchar
+ *
+ * Return: returns 0 always
+ *
+ */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
