1d0118a845a69d58278edc22513f6a9e81a458c7
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-05 19:15:05 +0000

    Print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+
+void print_alphabet_x10(void)
+{
+	char l;
+	int out;
+	
+	out = 0;
+
+	l = 'a';
+
+	while(out <= 10) {
+		while(l <= 'z')                                                                                                         {                                                                                                                               _putchar(l);                                                                                                            l++;                                                                                                            }
+		out++;
+}
