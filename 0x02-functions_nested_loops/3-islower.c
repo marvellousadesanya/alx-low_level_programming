67c748e4653d6d62cf7a76b5bba89e0c5621c701
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-05 20:18:32 +0000

    Lowercase or upper

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * _islower(int c) - Checks for lowercase
+ *
+ * Return 0 or 1
+ */
+int _islower(int c)
+{
+	if (islower(int c))
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
