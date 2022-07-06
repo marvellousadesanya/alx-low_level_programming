9fc74dfb954086198160ce90bec801f0c3fe64c0
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 09:47:35 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * abs - Entry point
+ *
+ * Description: 'Compute absolute value'
+ *
+ * @h: Checks for h
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int _abs(int h)
+{
+	putchar(h)
+	return (0);
+}
