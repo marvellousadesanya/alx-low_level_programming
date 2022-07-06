af6ecd1900e04d669bff1ea69dc7b6e3b8fd7c8e
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 19:36:50 +0000

    Hopefully correct

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Prints 24 hours digital clock
+ *
+ */
+
 void jack_bauer(void)
 {
 	int min, hr;
-	
+
 	hr = 0;
 	while (hr <= 23)
 	{
-		_putchar((hr / 10) + '0');
-		_putchar((hr % 10) + '0');
-		_putchar('\n');
-
 		min = 0;
 		while (min <= 59)
 		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
