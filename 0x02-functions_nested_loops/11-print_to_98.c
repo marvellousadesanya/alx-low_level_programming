ab9de0e25bda1766b3d2b4abb090e8115e7d1082
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 20:40:52 +0000

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,6 +19,13 @@ void print_to_98(int n)
 		_putchar(' ');
 		_putchar('\n');
 		n++;
-	}
+	} else if (n > 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+		n--;
 }
 
