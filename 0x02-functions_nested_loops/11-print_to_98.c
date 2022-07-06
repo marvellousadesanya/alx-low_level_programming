1232fa038593e0116de609c519b7e259977c2a39
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 21:37:33 +0000

    Solved

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,28 +2,21 @@
 
 void print_to_98(int n)
 {
-	if(n <= 0 && n <= 98)
+	if(n <= 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
-		n++;
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
-	else if (n > 0 && n <= 98)
+	else if (n > 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
-		n++;
-	} else if (n > 98)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
-		n--;
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
 	}
 }
 
