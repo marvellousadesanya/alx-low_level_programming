f06b6e5a8b9aa6db1eb04a0c98d19cfe7707bba9
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 10:10:36 +0000

    Return last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: 'Print last digit'
+ *
+ * @n: Checks for n
+ *
+ * Return: returns last digit
+ */
+
+int print_last_digit(int n)
+{
+	if (n % 10)
+	{
+		return (n);
+	}
+}
