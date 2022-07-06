e06dec0e55a2f598454bc38d8304cad439c9d77f
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 10:49:43 +0000

    Debugging

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,20 +2,11 @@
 
 void jack_bauer(void)
 {
-	int hr;
-
 	int min;
-
-	hr = 0;
-
-	while (hr <= 23)
+	
+	min = 0;
+	while (min <= 59)
 	{
-		min = 0;
-		while (min <= 59)
-		{
-			min++;
-		}
-		_putchar(hr + '0');
-		hr++;
-	}
+		_putchar(min + '0');
+		min++;
 }
