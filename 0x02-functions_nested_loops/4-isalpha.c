efb3bcee72164b7aac10a231a2b6d0a7de96b068
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 09:03:58 +0000

    Check for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: 'Checking for alphabet'
+ *
+ * @c: Checks for character c
+ *
+ * Return: 1 or 0;
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 097 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
