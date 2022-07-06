5efa1a05e371ae2e5eea166dfa25c78ae67d6a81
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 09:30:51 +0000

    Resolving errors

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
 		return (1);
+		_putchar('+');
 	} else if (n == 0)
 	{
-		putchar('0');
 		return (0);
+		_putchar('0')
 	} else
 	{
-		putchar('-');
 		return (-1);
+		_putchar('-');
 	}
 }
