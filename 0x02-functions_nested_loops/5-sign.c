095090097003fa5f32e015c6bda24feb234b5ecc
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-06 09:14:44 +0000

    Check for positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Description: 'Checking for negative or positive'
+ *
+ * @n: Check for n
+ *
+ * Return: Return 0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else
+		putchar('-');
+		return (0);
+}
