31d66c7e346d7fbc8424ba074d22e897f7ae5065
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-07 20:42:14 +0000

    Fix

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Nested loops to print series of numbers 10 times
+ */
+
+void more_numbers(void)
+{
+	char n;
+
+	char m;
+
+	n = '0';
+
+	while (n <= '9')
+	{
+		m = '0';
+		while (m <= '14')
+		{
+			_putchar(m);
+			m++;
+		}
+		_putchar(n);
+		n++;
+	}
+}
