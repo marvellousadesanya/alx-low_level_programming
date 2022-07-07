96b75a1433c76e510a35a18103c197087114593f
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-07 20:08:50 +0000

    Fixed an error

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,17 @@
 
 void print_most_numbers(void)
 {
-	int n;
+	char n;
 
 	n = 0;
 
-	while (n <= 9)
+	while (n <= '9')
 	{
 		if (n == '2' && n == '4')
+		{
 			continue;
-		_putchar(n);
-		n++;
+			_putchar(n);
+			n++;
+		}
 	}
 }
