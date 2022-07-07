cd9d6174e823b414c37099775503b8d331f8f489
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-07 21:44:47 +0000

    New algo with for loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,11 @@ void more_numbers(void)
 	{
 		for (m = 0; m <= 14; m++)
 		{
-			if (m >= 0)
-			_putchar('1');
-			_putchar(m % 10 + '0');
+			_putchar(m + '0');
+			if (m >= 10)
+				_putchar('1');
+				_putchar(m % 10 + '0');
+				m++;
 		}
 			_putchar('\n');
 	}
