98b22be87a6621ac6f3d7b1b4395a6df68b5c631
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-08 17:27:05 +0000

    Solved

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,20 +16,22 @@ int main(void)
 	{
 		if (m % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (m % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (m % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", m);
+			printf("%d", m);
 		}
+		if (m < 100)
+			print(" ");
 
 	}
 	return (0);
