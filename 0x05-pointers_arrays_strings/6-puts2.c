8d49fe709a5eb37a2fe66e1bd7bda94d9a0c6b96
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 21:30:29 +0000

    Slvd

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	if (str[i % 2 == 0])
-		_putchar(str[i]);
-	_putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
 }
