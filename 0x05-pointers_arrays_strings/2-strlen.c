014677a1f14046a1d20b4b1435aded9a3a155d59
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 10:21:17 +0000

    Finally solved

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,15 @@
 /**
  * _strlen - Checks for length
  * @s: Checks for character s
- * Returns the length
+ * Return: returns the length of string
  */
 
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
-	
+	for (i = 0; s[i] != '\0';
+			i++);
+
 	return (i);
 }
