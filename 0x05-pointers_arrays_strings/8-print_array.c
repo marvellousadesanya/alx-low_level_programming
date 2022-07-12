c0c203938e23df5d24aea2fc58edf28cf13c135c
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 22:53:34 +0000

    Change

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
+/**
+ * print_array - Prints an array
+ * @a: Checks for int a
+ * @n: Checks for int n
+ */
+
 void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+
+	if (i < n -1)
+	{
+		printf(", ");
+	}
 	printf("\n");
 }
