4a0731539a21be6061dc1eb0b9ef13cebeac0747
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 09:21:13 +0000

    Swapping 2 integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap 2 integers
+ *
+ * @a: Checks for int a 
+ *
+ * @b: Checks for int b
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int t;
+	int y;
+
+	t = *a;
+	y = *b;
+
+	t = *b;
+	y = *a;
+}
