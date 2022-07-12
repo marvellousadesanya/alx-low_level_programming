d7fb80a38b7f97fc6e62ce5f19892988f37cc3be
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 19:25:30 +0000

    hey

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,17 +5,20 @@ void rev_string(char *s)
 	int i = 0;
 	char reversedStr;
 	int j;
+	int k = 0;
 
 	while (s[i] != 0)
 	{
 		i++;
 	}
 	j = i - 1;
-	while (i >= 0)
+	while (j >= 0)
 	{
-		i--;
-		reversedStr[j++] = s;
+		j--;
+		reversedStr[k++] = s[j];
 	}
+	reversedStr[k] = '\0';
+
 	_putchar(s);
 	_putchar('\n');
 }
