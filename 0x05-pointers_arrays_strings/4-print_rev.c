8d032eeabc7f9e248b7304778e29f609bf64e110
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 12:02:40 +0000

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - Reverse a string
+ * 
+ * @s: Checks for char s
+ */
+
+void print_rev(char *s)
+{
+	for (; *str == 0; str--)
+	{
+		_putchar(*str);
+	}
+	_putchar(*str);
+}
