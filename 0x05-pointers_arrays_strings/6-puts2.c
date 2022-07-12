4071bfd27badefb33d7cdf9f3a34d20c3f677bdd
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-12 20:48:07 +0000

    Sup

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,10 +3,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j;
 
-	while(str[i % 2 == 0])
+	while (str[i] = '\0')
 	{
 		i++;
+	}
+
+	while(j = i - 1; str[i % 2 == 0]; j++)
+	{
 		_putchar(str[i]);
 	}
 }
