5a3b103ce51e73dbfe0c0ea7d8ec02624d569cc7
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-14 12:13:42 +0000

    Betty compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
+/**
+ * _strcat - concats two strings
+ * @dest: Checks for char dest
+ * @src: Checks for char src
+ * Return: Returns dest(the concated str)
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
+
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		; /** Not 100% sure why I left this here.
-		    * Might later remove */
+		    * Might later remove
+		    */
 	}
 
 	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
-		i++;
+		*dest += src[j];
 	}
 	*dest += '\0';
 
