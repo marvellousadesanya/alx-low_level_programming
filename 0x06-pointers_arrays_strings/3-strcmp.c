eea6a78dde62d01724c8c1033cf92665b61c664f
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-14 20:08:46 +0000

    Hopefully solved

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,21 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-		continue;
-
-	if (s1[i] == s2[i])
 	{
-		return (0);
+		if (s1[i] == s2[i])
+		{
+			j = 0;
+		}
+		else
+		{
+			j = (*s1 - *s2); /**
+					  * Returns 15. This is the difference
+					  * between the bytes in memory
+					  */
+		}
 	}
-	else
-	{
-		return (*s1 - *s2); /**
-				     * Returns 15. This is the difference
-				     * between the bytes in memory
-				     */
-	}
+		return (j);
 }
