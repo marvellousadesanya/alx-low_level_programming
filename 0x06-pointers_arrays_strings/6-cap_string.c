22b0bb4df143001ece2d5c38d759869df4c1e0a2
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-15 00:50:43 +0000

    Test

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes strings
+ * @s: Checks for char s
+ * Return: Returns the string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{','}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (sep[j] == s[i] && (s[i + 1] >= 97 && s[i + 1] <= 122))
+				s[i + 1] = s[i + 1] - 32;
+		}
+	}
+	return (s);
+}
