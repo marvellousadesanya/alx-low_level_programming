e6d5daee96c6a24906fce4061e9db058f91ec630
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-17 21:08:05 +0000

    My memset code

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the c to stdout
+ *
+ * @c: Checks for char c
+ *
+ * Return: On success 1.
+ *
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
