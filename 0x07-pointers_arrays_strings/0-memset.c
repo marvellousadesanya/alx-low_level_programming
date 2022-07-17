8179c3ff9dccd9952d05abd0ce6d05cb3fc4492b
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-17 21:12:21 +0000

    Betty compliant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * _memset - Fills memory with constant bytes
+ * @s: The buffer(pointer)
+ * @b: Character to be copied in memory
+ * @n: the n bytes of memory area to be pointed to.
+ * Return: Returns the pointer/buffer
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
