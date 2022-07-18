0c66c78e9d56a88b7e5289f5b4c8124a272275f6
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-18 09:28:28 +0000

    Return substring

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Get a substring
+ * @s: The buffer
+ * @c: Character to check for;
+ * Return: Substring(pointer) or NLLL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	/**
+	 * Length of string
+	 */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != c)
+			s++;
+		else
+			return (s);
+	}
+
+	return (NULL);
+}
