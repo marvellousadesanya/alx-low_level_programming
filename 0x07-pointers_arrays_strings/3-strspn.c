79f5d25ba5f6567c1c75f17b2f57bfd8d41d2cd9
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-18 13:56:58 +0000

    getting 7 instead

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,10 +15,16 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++) /* Loop throug the char s first */
 	{
-		for (j = 0; accept[j] != '\0'; j++) /* Loop through char accept now */
+		if (s[i] != 32) /**
+				  * !IMPORTANT: This ensures the string
+				  * is broken into segments
+				  */
 		{
-			if (s[i] == accept[j]) /* compares two string lengths */
-				newlen++; /* adds 1 each time condition is true */
+			for (j = 0; accept[j] != '\0'; j++) /* Loop through char accept now */
+			{
+				if (s[i] == accept[j]) /* compares two string lengths */
+					newlen++; /* adds 1 each time condition is true */
+			}
 		}
 
 	}
