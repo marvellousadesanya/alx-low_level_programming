f629825a9540f9c9caff16420ba79bf881357ba2
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-20 00:16:14 +0000

    First recursion task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Recursively prints out s
+ * @s: The character to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+		return; /* Must always return else we get wrong output */
+	}
+	_putchar(*s); /* Prints character */
+	_puts_recursion(s + 1); /* Adds char by 1 */
+}
