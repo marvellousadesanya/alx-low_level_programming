e629e4013712439c9a797e6cdb86175faa72b4f5
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-22 23:11:28 +0000

    Check number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main function with args
+ * @argc: Number of args passed
+ * @argv: Array containing all strings passed
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	int i, num;
+
+	for (i = 0; i < argc; i++)
+	{
+		num++;
+	}
+	printf("%d\n", num);
+	return (0);
+}
