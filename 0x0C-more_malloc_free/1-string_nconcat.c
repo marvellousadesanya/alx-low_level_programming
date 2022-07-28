7cadd5becb6cfa6c2fa0f34338ab2d2f523b5ae5
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-28 11:17:46 +0000

    Debugging

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		; /* len of s2 */
 
 	if (n >= len2)
-		n = len2;
+	{
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	}
 
 	s = malloc(sizeof(char) * (len1 + n + 1)); /* Assigned space */
 
