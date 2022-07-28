c38a172064860985d57b10d04c850d90dc2ec2b8
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-28 11:14:45 +0000

    Concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *s;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++); /* len of s1 */
-	for (len2 = 0; s2[len2] != '\0'; len2++); /* len of s2 */
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		; /* len of s1 */
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		; /* len of s2 */
 
 	if (n >= len2)
 		n = len2;
