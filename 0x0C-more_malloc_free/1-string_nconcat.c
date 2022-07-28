adfde15d61f6a0152cba9fe5472efe4c37992d4b
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-28 11:36:10 +0000

    Refactored

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,40 +10,40 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2;
-	unsigned int i, j;
+	unsigned int len1, len2, i, j;
 	char *s;
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		; /* len of s1 */
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		; /* len of s2 */
-
-	if (n >= len2)
+	if (s1 == NULL)
 	{
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		len1 = 0;
 	}
-
+	else
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			;
+	}
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	else
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
+			;
+	}
+	if (n >= len2)
+		n = len2;
 	s = malloc(sizeof(char) * (len1 + n + 1)); /* Assigned space */
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		s[i] = s1[i];
 	}
-
 	for (j = 0; j < n; j++)
 	{
 		s[i + j] = s2[j];
 	}
 	s[i + j] = '\0';
-
 	if (s == NULL)
 		return (NULL);
-
-	if (s1 == NULL)
-		len1 = 0;
-	if (s2 == NULL)
-		len2 = 0;
-
 	return (s);
 }
