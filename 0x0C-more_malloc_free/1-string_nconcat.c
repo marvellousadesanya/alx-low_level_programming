dcc21380f244868a20b77599853720f83f9b59b7
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-07-28 11:54:30 +0000

    Solved

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,14 +15,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-		
+
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-			;
+		;
+
 	if (s2 == NULL)
 		s2 = "";
-		
+
 	for (len2 = 0; s2[len2] != '\0'; len2++)
-			;
+		;
 	if (n >= len2)
 		n = len2;
 
