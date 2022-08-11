08fb5a495eeb928c8e9d814ec71e98a9e1ad94c9
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-08-11 18:39:46 +0000

    Adjustment

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,11 +8,11 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t n = 0; /* Initial val of num of elements in list */
 
 	if (h != NULL)
 	{
-		return ((count + 1) + list_len(h->next));
+		return ((n + 1) + list_len(h->next));
 	}
-	return (count);
+	return (n);
 }
