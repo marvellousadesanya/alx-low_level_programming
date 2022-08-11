ddce40a2a34e85035df3cc746decf8f37fff920f
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-08-11 18:36:43 +0000

    Solved

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,18 @@
 #include "lists.h"
 
+/**
+ * list_len - List elements in a list
+ * @h: Header node
+ * Return: The count
+ */
 
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h != NULL)
+	{
+		return ((count + 1) + list_len(h->next));
+	}
+	return (count);
+}
