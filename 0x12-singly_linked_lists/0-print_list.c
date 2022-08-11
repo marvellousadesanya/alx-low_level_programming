0e64e6241f29e49b58acd421bd64850bdb05c474
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-08-11 13:37:12 +0000

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - Print list of elements in Linked list
+ * @h: The head
+ * Return: Returns the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 1; /* Number of nodes */
+
+	if (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+		}
+		printf("\n");
+		print_list(h->next); /* Recursive function */
+		count++;
+	}
+	return (count); /* Exit function */
+}
