d087078262a73ff8f55a95fc437b29532f80ea2d
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-08-18 23:30:22 +0000

    solved

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - Convert dec to binary
+ * @n: Number
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+
+	for (i = 10; i >= 0; i--)
+	{
+		if (n & (1 << i))
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
